f77b57b85a9c6534aea8a5072627ff998d2b728d
409410544 <409410544@gms.tku.edu.tw>
2022-04-12 17:56:57 +0800

    initial commit

--- routes/crown2_44.js
+++ routes/crown2_44.js
@@ -1,9 +1,39 @@
 var express = require('express');
+const Category_44 = require('../models/Category_44');
+const Shop_44 = require('../models/Shop_44');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('crown2_44/index', { id: '409410544',title:'Crown2' });
-});
+  try {
+      let results = await Category_44.fetchAll();
+      console.log('result',JSON.stringify(results));
+      res.render('crown2_44/index', {
 
+        data: results,
+        id:'409410544',
+        title:'crown2'
+      });
+  }catch(err){
+    console.log(err);
+  }
+
+
+});
+ 
+router.get('/shop_44/:category',async function(req, res){
+  console.log('category',req.params.category);
+
+  try{
+    const cid = await category_44.fetchCatIdByName(req.params.category);
+    console.log('cid',cid);
+
+      let result = await Shop_44.fetchProductByCategory(cid);
+  console.log('result',JSON.stringify(results));
+
+  }catch(err){
+    console.log(err);
+  }
+
+});
 module.exports = router;
