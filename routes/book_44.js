6f3ed4e0b406a8df7a1f70f6adfac0960c18788e
409410544 <409410544@gms.tku.edu.tw>
2022-06-07 17:19:04 +0800

    w16-p3: /book_xx/create to book_xx/add_xx.ejs

--- routes/book_44.js
+++ routes/book_44.js
@@ -17,9 +17,22 @@ router.get('/',  async (req, res) => {
             name:'Angelo Lee' 
         });
     }catch(error){
-        console.log('error',error);
+        res.render('book_44/index', { 
+            data: '',
+            id: '409410544',
+            name:'Angelo Lee' 
+        });
     }
     
 });
 
+router.get('/create',   (req, res) => {
+ 
+    res.render('book_44/add_44', 
+    { 
+        id: '409410544',
+        name:'Angelo Lee' 
+    });
+    
+});
 module.exports = router;
